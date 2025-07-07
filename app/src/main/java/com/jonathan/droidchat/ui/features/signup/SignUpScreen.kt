4e968761d97be8ddf7197e2f7511392852719a57
jonathan <jonathan.wdev@gmail.com>
2025-07-07 18:32:05 -0300

    feat: readme

--- app/src/main/java/com/jonathan/droidchat/ui/features/signup/SignUpScreen.kt
+++ app/src/main/java/com/jonathan/droidchat/ui/features/signup/SignUpScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CornerSize
@@ -95,6 +96,7 @@ fun SignUpView(
     Box(
         modifier = Modifier
             .background(brush = BackgroundGradient)
+            .imePadding()
             .verticalScroll(state = scrollState)
     ) {
         Column(
